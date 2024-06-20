88b6f0b694d0d687cd6bc541669bfa625423cc89
cuongnc <cuongnc@unitech.vn>
2024-06-20 16:08:24 +0700

    ok

--- admin/layout/sidebar-left.php
+++ admin/layout/sidebar-left.php
@@ -10,15 +10,15 @@
                         <!-- /input-group -->
                     </li>
                     <li class="user-pro">
-                        <a href="#" class="waves-effect"><img src="../plugins/images/user.jpg" alt="user-img" class="img-circle"> <span class="hide-menu"> Account<span class="fa arrow"></span></span>
+                        <a href="#" class="waves-effect"><img src="../plugins/images/user.jpg" alt="user-img" class="img-circle"> <span class="hide-menu"> Tài khoản<span class="fa arrow"></span></span>
                         </a>
                         <ul class="nav nav-second-level">
-                            <li><a href="settings.php"><i class="ti-settings"></i> Account Setting</a></li>
-                            <li><a href="login.php"><i class="fa fa-power-off"></i> Logout</a></li>
+                            <li><a href="settings.php"><i class="ti-settings"></i> Cài đặt tài khoản</a></li>
+                            <li><a href="login.php"><i class="fa fa-power-off"></i> Thoát</a></li>
                         </ul>
                     </li>
-                    <li class="nav-small-cap m-t-10">--- Main Menu</li>
-                    <li> <a href="index.php" class="waves-effect"><i class="linea-icon linea-basic fa-fw" data-icon="v"></i> <span class="hide-menu"> Dashboard </a>
+                    <li class="nav-small-cap m-t-10">--- Danh mục</li>
+                    <li> <a href="index.php" class="waves-effect"><i class="linea-icon linea-basic fa-fw" data-icon="v"></i> <span class="hide-menu"> Thống kê </a>
                     </li>
                    
                     
@@ -31,17 +31,17 @@
                         </ul>
                     </li>
                    
-                   <li><a href="inbox.php" class="waves-effect"><i data-icon=")" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Messages</span></a>
+                   <li><a href="inbox.php" class="waves-effect"><i data-icon=")" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Tin nhắn</span></a>
                     </li>
 
-                    <li><a href="subscribers.php" class="waves-effect"><i data-icon="n" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Subscribers</span></a>
+                    <li><a href="subscribers.php" class="waves-effect"><i data-icon="n" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Khách hàng</span></a>
                     </li>
                     
-                     <li class="nav-small-cap">--- Other</li>
-                    <li> <a href="#" class="waves-effect active"><i data-icon="H" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Access<span class="fa arrow"></span></span></a>
+                     <li class="nav-small-cap">--- Hệ thống</li>
+                    <li> <a href="#" class="waves-effect active"><i data-icon="H" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Truy cập<span class="fa arrow"></span></span></a>
                         <ul class="nav nav-second-level">
-                            <li><a href="users.php">Administrators</a></li>
-                            <li><a href="new-user.php">Create Admin</a></li>
+                            <li><a href="users.php">Danh sách người dùng</a></li>
+                            <li><a href="new-user.php">Tạo người dùng</a></li>
                             
                         </ul>
                     </li>
